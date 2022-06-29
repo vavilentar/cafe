7ed1ff13248e66a3d5fd7b626add7e50246b9baa
Roman Pchelkin <epos.91@mail.ru>
2022-06-29 17:48:57 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const infoBoxes = document.querySelectorAll('.info-block');
+window.addEventListener('scroll', checkBoxes);
+
+function checkBoxes() {
+	const triggerBottom = window.innerHeight / 5 * 4;
+
+	infoBoxes.forEach((box) => {
+		const boxTop = box.getBoundingClientRect().top;
+
+		if (boxTop < triggerBottom) {
+			box.classList.add('box-show');
+		}
+		// } else {
+		// 	box.classList.remove('box-show');
+		// }
+		
+	});
+}
+
+//Slider
+
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
